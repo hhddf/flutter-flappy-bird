1bec9b225723a0fc689e51c07f281ab99ec83dfe
hhddf <57996116+hhddf@users.noreply.github.com>
2019-11-25 03:57:29 +0800

    Image positioning

--- flutter_flappy_bird/lib/main.dart
+++ flutter_flappy_bird/lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:async';
+import 'dart:async' show Timer;
+import 'package:flutter/material.dart';
 
 void log(content) {
   print(content);
@@ -12,7 +13,35 @@ void loop() {
 }
 
 void loopBody() {
-  log('loopBody');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: stack()
+      ),
+    );
+  }
+
+  Stack stack() {
+    Stack s = new Stack(
+      children: <Widget>[
+        new Positioned(
+          left: 30.0,
+          top: 30.0,
+          child: new Container(
+            width: 100.0,
+            height: 80.0,
+            child: Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+          ),
+        )
+      ],
+    );
+    return s;
+  }
 }
 
 void main() {
