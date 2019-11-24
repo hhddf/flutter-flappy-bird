862438e557fcfe2969e5ac95b1691303ba8df4a5
hhddf <57996116+hhddf@users.noreply.github.com>
2019-11-25 02:55:35 +0800

    Loop

--- flutter_flappy_bird/lib/main.dart
+++ flutter_flappy_bird/lib/main.dart
@@ -1,7 +1,20 @@
-void main() {
-  log('hello, world');
-}
+import 'dart:async';
 
 void log(content) {
   print(content);
+}
+
+void loop() {
+  Duration d = new Duration(milliseconds: 40);
+  Timer.periodic(d, (_) {
+    loopBody();
+  });
+}
+
+void loopBody() {
+  log('loopBody');
+}
+
+void main() {
+  loop();
 }
